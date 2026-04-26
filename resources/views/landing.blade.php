7fd20b42744007a135f62bb26bb016f20e4b54b7
VigoAlsabri <vigof9@gmail.com>
2026-04-26 17:08:13 +0700

    sistem air minum berbasis

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PAMSIMAS – Sistem Air Minum Berbasis Masyarakat</title>
+    <title>PAMSIMAS – Sistem Air Minum Berbasis </title>
     <script src="https://cdn.tailwindcss.com"></script>
     <script>
         tailwind.config = {
