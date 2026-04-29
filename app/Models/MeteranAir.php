03d7c6cbd1a19e33d20783e2bb3304a9bd4c8067
Muhammad Haris <mh723595@gmail.com>
2026-04-30 01:21:18 +0700

    memperbaiki error storage di model MeteranAir

--- app/Models/MeteranAir.php
+++ app/Models/MeteranAir.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class MeteranAir extends Model
 {
@@ -56,6 +57,6 @@ class MeteranAir extends Model
 
     public function fotoUrl(): ?string
     {
-        return $this->foto_meter ? \Storage::url($this->foto_meter) : null;
+        return $this->foto_meter ? Storage::url($this->foto_meter) : null;
     }
 }
