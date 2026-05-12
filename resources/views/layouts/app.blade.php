7d73fd1147b80ecd8ca4eb435a7dce3d7aa8a2f6
Muhammad Haris <mh723595@gmail.com>
2026-05-12 19:25:00 +0700

    Mengubah nama menu pengguna

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -148,7 +148,7 @@
             ['group'=>'Manajemen', 'items'=>[
                 ['r'=>'admin.pelanggan.index','l'=>'Pelanggan','i'=>'M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0z'],
                 ['r'=>'admin.petugas.index','l'=>'Petugas','i'=>'M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z M15 12a3 3 0 11-6 0 3 3 0 016 0z'],
-                ['r'=>'admin.users.index','l'=>'Pengguna','i'=>'M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z'],
+                ['r'=>'admin.users.index','l'=>'Manajemen User','i'=>'M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z'],
                 ['r'=>'admin.registrasi.index','l'=>'Pendaftaran','i'=>'M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z','badge'=>true],
             ]],
             ['group'=>'Keuangan', 'items'=>[
@@ -256,7 +256,7 @@
             <svg :class="sidebarCollapsed ? 'rotate-180' : ''" class="w-4 h-4 transition-transform duration-200" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 19l-7-7 7-7m8 14l-7-7 7-7"/>
             </svg>
-            <span x-show="!sidebarCollapsed" class="text-xs">Tutup Sidebar</span>
+            <span x-show="!sidebarCollapsed" class="text-xs"></span>
         </button>
     </div>
 </aside>
